e3455f0756092f1335b9e2d9958570f9ac279482
Nuria Rivas <00041517@uca.edu.sv>
2019-06-03 09:11:55 -0600

    Add UI

---
+++ app/src/main/java/com/numerica98/parcial1_pdm_00041517/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.numerica98.parcial1_pdm_00041517
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
