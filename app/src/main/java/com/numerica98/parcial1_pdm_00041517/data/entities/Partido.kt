fed7617152ade8f188efc3ff80d253d2c7ea08f9
Nuria Rivas <00041517@uca.edu.sv>
2019-06-19 15:14:11 -0600

    Arreglada la funcionalidad de guardado de partido y modificados los xmls

--- app/src/main/java/com/numerica98/parcial1_pdm_00041517/data/entities/Partido.kt
+++ app/src/main/java/com/numerica98/parcial1_pdm_00041517/data/entities/Partido.kt
@@ -1,5 +1,7 @@
 package com.numerica98.parcial1_pdm_00041517.data.entities
 
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
@@ -8,12 +10,38 @@ import androidx.room.PrimaryKey
 data class Partido(
     @ColumnInfo(name = "nombre_equipo1") var nombre_equipo1: String,
     @ColumnInfo(name = "nombre_equipo2") var nombre_equipo2: String,
-    @ColumnInfo(name = "puntos_equipo1") var puntos_equipo1: Int,
-    @ColumnInfo(name = "puntos_equipo2") var puntos_equipo2: Int,
+    @ColumnInfo(name = "puntos_equipo1") var puntos_equipo1: String,
+    @ColumnInfo(name = "puntos_equipo2") var puntos_equipo2: String,
     @ColumnInfo(name = "hora") var hora: String,
     @ColumnInfo(name = "fecha") var fecha: String,
     @ColumnInfo(name = "ganador") var ganador: String
-) {
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        nombre_equipo1 = parcel.readString(),
+        nombre_equipo2 = parcel.readString(),
+        puntos_equipo1 = parcel.readString(),
+        puntos_equipo2 = parcel.readString(),
+        hora = parcel.readString(),
+        fecha = parcel.readString(),
+        ganador = parcel.readString())
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(nombre_equipo1)
+        parcel.writeString(nombre_equipo2)
+        parcel.writeString(puntos_equipo1)
+        parcel.writeString(puntos_equipo2)
+        parcel.writeString(hora)
+        parcel.writeString(fecha)
+        parcel.writeString(ganador)
+    }
+
+    override fun describeContents() = 0
+
+    companion object CREATOR : Parcelable.Creator<Partido> {
+        override fun createFromParcel(parcel: Parcel): Partido = Partido(parcel)
+        override fun newArray(size: Int): Array<Partido?> = arrayOfNulls(size)
+    }
+
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "id")
     var id: Int = 0
